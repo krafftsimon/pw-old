d48b76248bf257d77568acd8d6e37930a6b8c283
simon <krafftsimon@gmail.com>
2018-03-07 14:06:59 -0500

    compressed dist folder

--- my-app/server.js
+++ my-app/server.js
@@ -2,10 +2,30 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
+
+app.get('*.js', (req, res, next) => {
+  if ('/inline' === req.url.substring(0,7)) {
+    next();
+  } else {
+    req.url = req.url + '.gz';
+    res.set('Content-Encoding', 'gzip');
+    res.set('Content-Type', 'application/javascript');
+    next();
+  }
+})
+
+app.get('*.css', (req, res, next) => {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', 'text/css');
+  next();
+})
+
 // Serve static files
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Return other routes to Angular index file..
+// Return other routes to Angular index file.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
